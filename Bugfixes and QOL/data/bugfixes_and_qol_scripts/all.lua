f64ce1b76d1b5804aafaa03b5e31e657693c75a7
CommunistMountain <greencrosshair@gmail.com>
2025-11-27 13:56:12 +0800

    Fixed timers

--- Bugfixes and QOL/data/bugfixes_and_qol_scripts/all.lua
+++ Bugfixes and QOL/data/bugfixes_and_qol_scripts/all.lua
@@ -1,18 +1,41 @@
+local all_time_increments = 0
+local no_pause_time_increments = 0
+
+local function time_increment_cm()
+    if Hyperspace.FPS.speedLevel ~= 0 and Hyperspace.FPS.speedEnabled then
+        return Hyperspace.FPS.SpeedFactor/16
+    elseif Hyperspace.FPS.NumFrames ~= 0 then
+        return 1/Hyperspace.FPS.NumFrames
+    else
+        return 0
+    end
+end
+
 script.on_game_event("START_BEACON", false, function()
     if Hyperspace.App.world.starMap.currentSector.level == 0 then
-        Hyperspace.metaVariables.all_ticks_cm = 0
-        Hyperspace.metaVariables.no_pause_ticks_cm = 0
+        Hyperspace.metaVariables.all_time_cm = 0
+        all_time_increments = 0
+        Hyperspace.metaVariables.no_pause_time_cm = 0
+        no_pause_time_increments = 0
     end
 end)
 
 script.on_render_event(Defines.RenderEvents.FTL_BUTTON, function() end, function()
-    Hyperspace.metaVariables.all_ticks_cm = Hyperspace.metaVariables.all_ticks_cm + 1
+    all_time_increments = all_time_increments + time_increment_cm()
+    if all_time_increments > 1 then
+        all_time_increments = all_time_increments - 1
+        Hyperspace.metaVariables.all_time_cm = Hyperspace.metaVariables.all_time_cm + 1
+    end
 end)
 
 script.on_internal_event(Defines.InternalEvents.SHIP_LOOP, function(shipManager)
-    if shipManager.iShipId == 0 then  -- for all player-specific checks
-        Hyperspace.metaVariables.no_pause_ticks_cm = Hyperspace.metaVariables.no_pause_ticks_cm + 1
-    
+    if shipManager.iShipId == 0 then -- for all player-specific checks
+        no_pause_time_increments = no_pause_time_increments + time_increment_cm()
+        if no_pause_time_increments > 1 then
+            no_pause_time_increments = no_pause_time_increments - 1
+            Hyperspace.metaVariables.no_pause_time_cm = Hyperspace.metaVariables.no_pause_time_cm + 1
+        end
+        
         local hackingSystem = shipManager.hackingSystem
         if hackingSystem ~= nil and hackingSystem.spendDrone == 1 then
             hackingSystem.spendDrone = 0 -- don't use this method, directly modify drone count
@@ -28,14 +51,12 @@ script.on_internal_event(Defines.InternalEvents.SHIP_LOOP, function(shipManager)
         end
     else -- elseif shipManager.iShipId == 1 then -- for all enemy-specific checks
         if shipManager.ship.bDestroyed and shipManager.ship.shipName == "boss_3" then
-            local all_ticks = Hyperspace.metaVariables.all_ticks_cm
-            local all_minutes = all_ticks // 1800
-            local all_seconds = all_ticks % 1800 / 30
-            local no_pause_ticks = Hyperspace.metaVariables.no_pause_ticks_cm
-            local no_pause_minutes = no_pause_ticks // 1800
-            local no_pause_seconds = no_pause_ticks % 1800 / 30
-            print(string.format("Time including pauses: %d min %.2f s (%d ticks)", all_minutes, all_seconds, all_ticks))
-            print(string.format("Time excluding pauses: %d min %.2f s (%d ticks)", no_pause_minutes, no_pause_seconds, no_pause_ticks))
+            local all_minutes = Hyperspace.metaVariables.all_time_cm // 60
+            local all_seconds = Hyperspace.metaVariables.all_time_cm % 60 + all_time_increments
+            local no_pause_minutes = Hyperspace.metaVariables.no_pause_time_cm // 60
+            local no_pause_seconds = Hyperspace.metaVariables.no_pause_time_cm % 60 + no_pause_time_increments
+            print(string.format("Time including pauses: %d min %.2f s", all_minutes, all_seconds))
+            print(string.format("Time excluding pauses: %d min %.2f s", no_pause_minutes, no_pause_seconds))
         end
     end
 end)
